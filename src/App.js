eccc2a1978d8fd528aeda15bab2c8211e16e888e
dhanashreesawant <73907654+dhanashreesawant@users.noreply.github.com>
2020-11-04 10:48:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import './App.css';
+import { Component } from 'react';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from './components/KeypadComponent';
+class App extends Component{
+  constructor(){
+      super();
+
+      this.state = {
+          result: ""
+      }
+  }
+
+  onClick=button=>{
+    if(button==="="){
+      this.calculate()
+    }
+    else if(button==="C"){
+      this.reset()
+    }
+    else if(button==="CE"){
+      this.backspace()
+    }
+    else{
+      this.setState({
+        result:this.state.result+button
+      })
+    }
+  };
+calculate= ()=>{
+  try{
+    this.setState({
+      result:(eval(this.state.result)||"")+""
+    })
+
+  }
+  catch(e){
+this.setState({
+  result:"error"
+})
+  }
+};
+reset=()=>{
+  this.setState({
+    result:""
+  })
+}
+backspace=()=>{
+  this.setState({
+    result:this.state.result.slice(0,-1)
+  })
+};
+render(){
+
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Calculator</h1>
+<ResultComponent result={this.state.result}/>
+<KeyPadComponent onClick={this.onClick}/>
+      </div>
+       
+    
+    </div>
+  );
+}
+
+}
+export default App;
